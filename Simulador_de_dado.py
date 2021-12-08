694d8066d75384f8b9e449d0295701fef01d00d7
Aristeu-Dantas <aristeu_dantas@hotmail.com>
2021-12-08 16:32:23 -0300

    Primeiro commit

---
+++ Simulador_de_dado.py
@@ -0,0 +1,28 @@
+#Simulando o uso de dados de 1 até 6
+import random
+
+class SimuladorDeDado:
+    def __init__(self):
+        self.valor_minimo: 1
+        self.valor_maximo: 6
+        self.mensagem = 'Você gostaria de gerar um valor para os dados?'
+
+    def Iniciar(self):
+        resposta = input(self.mensagem)
+        try:
+
+            if resposta == 'sim':
+                self.GerarValorDoDado()
+            elif resposta == 'não':
+                print('Volte sempre!')
+            else:
+                print('Digite sim ou não')
+        except:
+            print('Ocorreu um erro em sua resposta')
+
+
+    def GerarValorDoDado(self):
+        print(random.randint(self.valor_minimo,self.valor_maximo))
+
+simulador = SimuladorDeDado()
+simulador.Iniciar()
