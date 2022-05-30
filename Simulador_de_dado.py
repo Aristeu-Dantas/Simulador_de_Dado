d32636cd7981ea585cd4133347914d4d31776789
Aristeu Dantas <82111816+Aristeu-Dantas@users.noreply.github.com>
2022-05-29 21:36:41 -0300

    Update Simulador_de_dado.py

--- Simulador_de_dado.py
+++ Simulador_de_dado.py
@@ -1,16 +1,18 @@
 #Simulando o uso de dados de 1 até 6
 import random
 
+#Definindo parametros
 class SimuladorDeDado:
     def __init__(self):
         self.valor_minimo: 1
         self.valor_maximo: 6
         self.mensagem = 'Você gostaria de gerar um valor para os dados?'
 
+#Recebendo comando
     def Iniciar(self):
         resposta = input(self.mensagem)
         try:
-
+#Inicializando            
             if resposta == 'sim':
                 self.GerarValorDoDado()
             elif resposta == 'não':
